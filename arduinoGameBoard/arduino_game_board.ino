8ada9977453a534cdbdd78b7d0268e80137527b0
davwys <david.wyss@hotmail.ch>
2019-11-06 14:08:04 +0100

    add status setup

--- arduinoGameBoard/arduino_game_board.ino
+++ arduinoGameBoard/arduino_game_board.ino
@@ -1,30 +1,62 @@
 #include <Arduino.h>
 #include "User_Setup.h"
 
-String incomingBytes = ""; // for incoming serial data
+/*  ==========================
+    Status Enum for game board
+    ==========================
+
+    READY       = ready for training or game start
+    CONNECTED   = connected to training dashboard, not training yet (serial port open - prevents game start)
+    TRAINING    = training new data via dashboard (sends sensor inputs to dashboard via specified port)
+    PLAYING     = actively playing the game (outputs API calls for game logic)
+ */
+
+enum status {
+    READY,
+    CONNECTED,
+    TRAINING,
+    PLAYING
+};
+
+
+/* ==================
+   Physical pin setup
+   ================== */
+
+//LED Pins
 int LED_Red = 3;
 int LED_Green = 4;
+
+/* =====================
+   Logic variables setup
+   ===================== */
+
+String receivedData = "";       //Incoming Serial data string
+status currentStatus = READY;   //Game board's current status
+
 
 void setup() {
     pinMode(LED_BUILTIN, OUTPUT);
     pinMode(LED_Red, OUTPUT);
     pinMode(LED_Green, OUTPUT);
     Serial.begin(57600);
+
+
 }
 
 void loop() {
 
     if (Serial.available() > 0) {
         // read the incoming data:
-        incomingBytes = Serial.readStringUntil('\n');
+        receivedData = Serial.readStringUntil('\n');
 
-        Serial.print(incomingBytes);
+        Serial.print(receivedData);
 
-        if(incomingBytes.indexOf("test") > -1)
+        if(receivedData.indexOf("test") > -1)
         {
             digitalWrite(LED_Green, HIGH);   // turn the LED on
             delay(500);                       // wait for half a second
-            digitalWrite(LED_Green, LOW);    // turn the LED offasdf
+            digitalWrite(LED_Green, LOW);    // turn the LED off
         }
         else{
             digitalWrite(LED_Red, HIGH);   // turn the LED on
