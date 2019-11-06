673a6406835bfd5b1fd54d80dece4018e08be272
davwys <david.wyss@hotmail.ch>
2019-11-06 15:44:11 +0100

    switch to atom

--- arduinoGameBoard/cmake-build-debug/compiler-file12597687032161718563.d
+++
@@ -1,2 +0,0 @@
-compiler-file12597687032161718563.o: \
- /private/var/folders/wr/fzy7p6d96rsb_qw27fqbzf8c0000gn/T/compiler-file12597687032161718563
--- arduinoGameBoard/cmake-build-debug/compiler-file5226783593859673342.d
+++
@@ -1,2 +0,0 @@
-compiler-file5226783593859673342.o: \
- /private/var/folders/wr/fzy7p6d96rsb_qw27fqbzf8c0000gn/T/compiler-file5226783593859673342
