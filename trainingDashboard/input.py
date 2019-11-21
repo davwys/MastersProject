51f4f45848784e02790336137cb8bc012cd9e6c9
unknown <vincent@rueegge.ch>
2019-11-21 11:46:27 +0100

    area name check

--- trainingDashboard/input.py
+++ trainingDashboard/input.py
@@ -1,5 +1,6 @@
 import serial
 import serial.tools.list_ports
+import re
 
 
 # Enables user input for an area name after a card has been played
@@ -14,6 +15,7 @@ def request_area_name(self):
 # Saves the area name given by the user (if length is within a valid range)
 # and sends "TRAIN_OK" message to request new training data
 def submit_area_name(self, name):
+    characters = re.compile('[@_&!#$%^*()<>?/\|{}~:;]')
     if 0 < len(name) < 20:
         self.ids.area_name.hint_text = ''
         self.ids.area_name.disabled = True
@@ -29,5 +31,7 @@ def submit_area_name(self, name):
         # Enable upload after we have at least one row of data
         self.ids.upload.disabled = False
         self.update_log('Area {} has been activated'.format(name))
+    elif characters.search(name) is not None:
+        self.update_log('Invalid area name')
     else:
         self.update_log('Error: Name too long')
