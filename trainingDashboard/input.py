0fc6dbc2c413dcbf9a0324c8bc6822d980b2b361
unknown <vincent@rueegge.ch>
2019-11-29 11:02:44 +0100

    area name whitelist

--- trainingDashboard/input.py
+++ trainingDashboard/input.py
@@ -15,8 +15,7 @@ def request_area_name(self):
 # Saves the area name given by the user (if length is within a valid range)
 # and sends "TRAIN_OK" message to request new training data
 def submit_area_name(self, name):
-    characters = re.compile('[@_&!#$%^*()<>?/\|{}~:;]')
-    if 0 < len(name) < 20 and characters.search(name) is None:
+    if 0 < len(name) < 20 and re.match('^[a-zA-Z0-9]', name):
         self.ids.area_name.hint_text = ''
         self.ids.area_name.disabled = True
         self.ids.area_name.text = ''
@@ -31,7 +30,7 @@ def submit_area_name(self, name):
         # Enable upload after we have at least one row of data
         self.ids.upload.disabled = False
         self.update_log('Area {} has been activated'.format(name))
-    elif characters.search(name) is not None:
+    elif not re.match('^[a-zA-Z0-9]', name):
         self.update_log('Invalid area name')
         self.ids.submit_name.disabled = False
 
